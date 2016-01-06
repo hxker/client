8d0eda7c926466fec99f943d4da494fbda08c4b6
huaxiukun <huaxiukun@yiyc.com>
2016-01-06 17:11:52 +0800

    reduce redirect

--- lib/sso.rb
+++ lib/sso.rb
@@ -6,42 +6,9 @@ module OmniAuth
       CUSTOM_PROVIDER_URL = 'http://localhost:3000'
 
       option :client_options, {
-        :site =>  CUSTOM_PROVIDER_URL,
-        :authorize_url => "#{CUSTOM_PROVIDER_URL}/auth/login/authorize",
-        :access_token_url => "#{CUSTOM_PROVIDER_URL}/auth/login/access_token"
-      }
-
-      uid do
-        raw_info['id']
-      end
-
-      info do
-        {
-          :email => raw_info['info']['email']
-        }
-      end
-
-      extra do
-        {
-          :first_name => raw_info['extra']['first_name'],
-          :last_name  => raw_info['extra']['last_name']
-        }
-      end
-
-      def raw_info
-        @raw_info ||= access_token.get("/auth/login/user.json?oauth_token=#{access_token.token}").parsed
-      end
-    end
-
-
-    class Register < OmniAuth::Strategies::OAuth2
-
-      CUSTOM_PROVIDER_URL = 'http://localhost:3000'
-
-      option :client_options, {
                                 :site => CUSTOM_PROVIDER_URL,
-                                :authorize_url => "#{CUSTOM_PROVIDER_URL}/auth/register/authorize",
-                                :access_token_url => "#{CUSTOM_PROVIDER_URL}/auth/register/access_token"
+                                :authorize_url => "#{CUSTOM_PROVIDER_URL}/auth/login/authorize",
+                                :access_token_url => "#{CUSTOM_PROVIDER_URL}/auth/login/access_token"
                             }
 
       uid do
@@ -62,8 +29,41 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get("/auth/register/user.json?oauth_token=#{access_token.token}").parsed
+        @raw_info ||= access_token.get("/auth/login/user.json?oauth_token=#{access_token.token}").parsed
       end
+    end
+
+
+    class Register < OmniAuth::Strategies::OAuth2
+
+      #   CUSTOM_PROVIDER_URL = 'http://localhost:3000'
+      #
+      #   option :client_options, {
+      #                             :site => CUSTOM_PROVIDER_URL,
+      #                             :authorize_url => "#{CUSTOM_PROVIDER_URL}/auth/register/authorize",
+      #                             :access_token_url => "#{CUSTOM_PROVIDER_URL}/auth/register/access_token"
+      #                         }
+      #
+      #   uid do
+      #     raw_info['id']
+      #   end
+      #
+      #   info do
+      #     {
+      #         :email => raw_info['info']['email']
+      #     }
+      #   end
+      #
+      #   extra do
+      #     {
+      #         :first_name => raw_info['extra']['first_name'],
+      #         :last_name => raw_info['extra']['last_name']
+      #     }
+      #   end
+      #
+      #   def raw_info
+      #     @raw_info ||= access_token.get("/auth/register/user.json?oauth_token=#{access_token.token}").parsed
+      #   end
     end
 
   end
